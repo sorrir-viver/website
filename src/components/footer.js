0697e91d09fc1e930e1038a73a629283eae0d0e0
Ciro Cesar Maciel <me@ciro-maciel.me>
2019-04-10 08:36:49 -0300

    - novos icones (svg) - formatação correta

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,11 +14,22 @@ export default function(props) {
         width: '100%',
         padding: '0 10px',
       }}>
-      <Col span={24} style={{ alignItems: 'center', display: 'flex' }}>
-        <div style={{ marginLeft: 'auto', fontSize: '11px' }}>
-          <strong>Sorrir & Viver - 2019 &copy; + &reg;</strong>
-        </div>
-      </Col>
+      <Row style={{ width: '1200px', padding: '10px 0', alignItems: 'center', display: 'flex' }}>
+        <Col span={24} style={{ alignItems: 'center', display: 'flex' }}>
+          <div>
+            <a href="https://www.instagram.com/sorrir.viver/" target="_blank">
+              Instagram
+            </a>
+            &nbsp;
+            <a href="https://www.facebook.com/sorrir.viver" target="_blank">
+              Facebook
+            </a>
+          </div>
+          <div style={{ marginLeft: 'auto', fontSize: '11px' }}>
+            <strong>Sorrir & Viver - {new Date().getFullYear()} &copy; + &reg;</strong>
+          </div>
+        </Col>
+      </Row>
     </Row>
   );
 }
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Row, Col, Typography } from 'antd';
 
-import homeOrofacial from '../../assets/img/home-orofacial.png';
+import homeMarca from '../../assets/img/home-marca.svg';
+import homeServico from '../../assets/img/home-servico.svg';
+import homeOrofacial from '../../assets/img/home-orofacial.svg';
+import homeGerenciamento from '../../assets/img/home-gerenciamento.svg';
 
 import Header from '../../components/header';
 import Body from '../../components/body';
@@ -13,23 +16,15 @@ export default (props) => [
     <Row style={{ width: '1200px', padding: '10px' }}>
       <Row type="flex">
         <Col span={24} style={{ textAlign: 'center', margin: '30px 0px' }}>
-          <Typography.Title level={2}>
-            Descubra como nós da Sorrir & Viver <br />
+          <Typography.Title level={2} style={{color: "#676c53f3"}}>
+            Descubra como nós da <br />
+            <span style={{ fontSize: '30px' }}>Sorrir & Viver</span> <br />
             podemos transformar seu desempenho nos <br />
             Negócios... e Vida...
           </Typography.Title>
         </Col>
-        <Col span={24}>
-          <Col
-            xs={24}
-            md={12}
-            style={{
-              textAlign: 'center',
-              height: '450px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+        <Col span={24} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }}>
+          <Col xs={24} md={12} style={{ textAlign: 'center', padding: '30px 0' }}>
             <div>
               <Typography.Title level={3}>Registre sua Marca</Typography.Title>
               <p style={{ fontSize: '16px' }}>
@@ -42,42 +37,19 @@ export default (props) => [
             xs={24}
             md={12}
             style={{
-              height: '450px',
-              textAlign: 'center',
+              height: '380px',
               display: 'flex',
               justifyContent: 'center',
               alignItems: 'center',
             }}>
-            <img src={homeOrofacial} />
+            <img src={homeMarca} />
           </Col>
         </Col>
-        <Col
-          span={24}
-          style={{
-            backgroundColor: 'rgb(247, 247, 247)',
-          }}>
-          <Col
-            xs={24}
-            md={12}
-            style={{
-              textAlign: 'center',
-              height: '450px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            wqwqw
+        <Col span={24} style={{ backgroundColor: 'rgb(247, 247, 247)', display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }}>
+          <Col xs={24} md={12} style={{ textAlign: 'center', height: '380px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img src={homeServico} />
           </Col>
-          <Col
-            xs={24}
-            md={12}
-            style={{
-              textAlign: 'center',
-              height: '450px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+          <Col xs={24} md={12} style={{ textAlign: 'center', padding: '30px 0' }}>
             <div>
               <Typography.Title level={3}>Aprenda a calcular o preço de seus Serviços</Typography.Title>
               <p style={{ fontSize: '16px' }}>
@@ -86,17 +58,8 @@ export default (props) => [
             </div>
           </Col>
         </Col>
-        <Col span={24} style={{ height: '450px' }}>
-          <Col
-            xs={24}
-            md={12}
-            style={{
-              textAlign: 'center',
-              height: '450px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+        <Col span={24} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }}>
+          <Col xs={24} md={12} style={{ textAlign: 'center', padding: '30px 0' }}>
             <div>
               <Typography.Title level={3}>
                 Atendendo pacientes de <br />
@@ -109,42 +72,15 @@ export default (props) => [
               </p>
             </div>
           </Col>
-          <Col
-            xs={24}
-            md={12}
-            style={{
-              textAlign: 'center',
-              height: '450px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+          <Col xs={24} md={12} style={{ textAlign: 'center', height: '380px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <img src={homeOrofacial} />
           </Col>
         </Col>
-        <Col span={24} style={{ backgroundColor: 'rgb(247, 247, 247)', height: '450px' }}>
-          <Col
-            xs={24}
-            md={12}
-            style={{
-              textAlign: 'center',
-              height: '450px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            wqwqw
+        <Col span={24} style={{ backgroundColor: 'rgb(247, 247, 247)', display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }}>
+          <Col xs={24} md={12} style={{ textAlign: 'center', height: '380px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img src={homeGerenciamento} />
           </Col>
-          <Col
-            xs={24}
-            md={12}
-            style={{
-              textAlign: 'center',
-              height: '450px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+          <Col xs={24} md={12} style={{ textAlign: 'center', padding: '30px 0' }}>
             <div>
               <Typography.Title level={3}>
                 Quer automatizar o Gerenciamento <br />
